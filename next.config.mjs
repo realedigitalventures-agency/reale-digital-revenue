bbf460dcac6b238096ca6f55eaa5b50407f36bf2
realedigitalventures-agency <realedigitalventures@gmail.com>
2026-02-25 10:53:36 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  experimental: {
+    optimizePackageImports: ["three"]
+  },
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  }
 };
 
 export default nextConfig;
